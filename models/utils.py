c1d543a27745889fd5a946dc2cd84c46636de0a4
Ivan Drokin <i.drokin@prequelapp.com>
2024-05-19 13:55:51 +0300

    add l1 decay

---
+++ models/utils.py
@@ -0,0 +1,110 @@
+# Based on this implementations: https://github.com/szymonmaszke/torchlayers/blob/master/torchlayers/regularization.py
+import abc
+
+import torch
+import torch.nn as nn
+
+
+class WeightDecay(nn.Module):
+    def __init__(self, module, weight_decay, name: str = None):
+        if weight_decay <= 0.0:
+            raise ValueError(
+                "Regularization's weight_decay should be greater than 0.0, got {}".format(
+                    weight_decay
+                )
+            )
+
+        super().__init__()
+        self.module = module
+        self.weight_decay = weight_decay
+        self.name = name
+
+        self.hook = self.module.register_full_backward_hook(self._weight_decay_hook)
+
+    def remove(self):
+        self.hook.remove()
+
+    def _weight_decay_hook(self, *_):
+        if self.name is None:
+            for param in self.module.parameters():
+                if param.grad is None or torch.all(param.grad == 0.0):
+                    param.grad = self.regularize(param)
+        else:
+            for name, param in self.module.named_parameters():
+                if self.name in name and (
+                    param.grad is None or torch.all(param.grad == 0.0)
+                ):
+                    param.grad = self.regularize(param)
+
+    def forward(self, *args, **kwargs):
+        return self.module(*args, **kwargs)
+
+    def extra_repr(self) -> str:
+        representation = "weight_decay={}".format(self.weight_decay)
+        if self.name is not None:
+            representation += ", name={}".format(self.name)
+        return representation
+
+    @abc.abstractmethod
+    def regularize(self, parameter):
+        pass
+
+
+class L2(WeightDecay):
+    r"""Regularize module's parameters using L2 weight decay.
+
+    Example::
+
+        import torchlayers as tl
+
+        # Regularize only weights of Linear module
+        regularized_layer = tl.L2(tl.Linear(30), weight_decay=1e-5, name="weight")
+
+    .. note::
+            Backward hook will be registered on `module`. If you wish
+            to remove `L2` regularization use `remove()` method.
+
+    Parameters
+    ----------
+    module : torch.nn.Module
+        Module whose parameters will be regularized.
+    weight_decay : float
+        Strength of regularization (has to be greater than `0.0`).
+    name : str, optional
+        Name of parameter to be regularized (if any).
+        Default: all parameters will be regularized (including "bias").
+
+    """
+
+    def regularize(self, parameter):
+        return self.weight_decay * parameter.data
+
+
+class L1(WeightDecay):
+    """Regularize module's parameters using L1 weight decay.
+
+    Example::
+
+        import torchlayers as tl
+
+        # Regularize all parameters of Linear module
+        regularized_layer = tl.L1(tl.Linear(30), weight_decay=1e-5)
+
+    .. note::
+            Backward hook will be registered on `module`. If you wish
+            to remove `L1` regularization use `remove()` method.
+
+    Parameters
+    ----------
+    module : torch.nn.Module
+        Module whose parameters will be regularized.
+    weight_decay : float
+        Strength of regularization (has to be greater than `0.0`).
+    name : str, optional
+        Name of parameter to be regularized (if any).
+        Default: all parameters will be regularized (including "bias").
+
+    """
+
+    def regularize(self, parameter):
+        return self.weight_decay * torch.sign(parameter.data)
